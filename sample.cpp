f9b8991ec98f6a503933ade6d08623df83e8c479
omkar <omkarmahajan1702@gmail.com>
2025-09-06 17:40:52 +0530

    main commit

--- sample.cpp
+++ sample.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     int a, b;
-    cout << "enter the nums :";
+    cout << "enter the nums in main:";
     cin >> a >> b;
     int sum = a + b;
     cout << "the sum is :" << sum;
