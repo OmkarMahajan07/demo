85ab1c52e504e8488c0a4243f3ff1c65ff685cbf
omkar <omkarmahajan1702@gmail.com>
2025-09-06 12:35:26 +0530

    initial commit

---
+++ sample.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "enter the numbers :";
+    cin >> a >> b;
+    int sum = a + b;
+    cout << "the sum is :" << sum;
+} // namespace std
